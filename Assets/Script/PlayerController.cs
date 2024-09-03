9cba895cd9bf39e79feefbfee91881df9c08dc76
Dev maheshwari <devmaheswari2017@gmail.com>
2024-09-03 19:02:33 +0530

    Added Garage Front door animation

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private float lookRotationSpeed;
+    [SerializeField] private float interactRange = 2f;
+
+    [Header("UI")]
+    [SerializeField] private Image interactionTimeWheel;
+
+    [Header("Scripts Refrence")]
+    [SerializeField] Interactable interactable;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private const string IDLE = "Idle";
+    [SerializeField] private const string WALK = "Walk";
+
+    [Header("Movement")]
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private LayerMask clickAbleLayers;
+    [SerializeField] private ParticleSystem clickEffect;
+    [SerializeField] private float smoothFactor;
+    [SerializeField] private float moveBackDistance= 5f;
+
+    private PlayerInput input;
+    private float holdDuration = 2.0f;
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+    private void Awake()
+    {
+        input = new PlayerInput();
+        AssignInputs();
+    }
+
+    private void Update()
+    {
+        FaceTarget();
+        SetAnimation();
+        CheckForInteractables();      
+    }
+
+    private void CheckForInteractables()
+    {
+        float remaningDuration;
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Stationary)
+            {
+                remaningDuration = holdDuration -= Time.deltaTime;
+                interactionTimeWheel.fillAmount = Mathf.Clamp01(remaningDuration / holdDuration);
+                Debug.Log(" duration " + holdDuration);
+            }
+            if (holdDuration <= 0 && touch.phase == TouchPhase.Ended)
+            {
+
+                Debug.Log("touch ended");
+                Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+                foreach (Collider collider in colliderArray)
+                {
+                    if (collider.TryGetComponent(out Interactable interactable))
+                    {
+                        Debug.Log("Found a interactable object");
+                        interactable.DoorInteraction();
+                    }
+
+                }
+            }
+        }
+
+        else { holdDuration = 2f; }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        Interactable interactable = collision.GetComponent<Interactable>();
+        if (interactable != null && collision.gameObject.CompareTag("Interactables"))
+        {
+            interactable.ShowInteractionPanel();
+        }
+        else if(interactable != null)
+        {
+            Debug.Log("colliding with " + collision.gameObject);
+            interactable.HideInteractionPanel();
+        }
+        if ((clickAbleLayers.value & (1 << collision.gameObject.layer)) == 0)
+        {
+            agent.isStopped = true;
+            //StartCoroutine(MoveBackAfterCollision());
+        }
+    }
+    private void FaceTarget() 
+    {
+        Vector3 direction = (agent.destination - transform.position).normalized;
+        if (direction != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
+        }
+    }
+    private void AssignInputs()
+    {
+        input.Touch.TouchMovement.performed += ctx => ClickToMove();
+    }
+
+    private void ClickToMove()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, 25, clickAbleLayers))
+        {
+                agent.isStopped = false;
+                agent.destination = hit.point;
+                if (clickEffect != null)
+                {
+                    ParticleSystem _clickEffect = Instantiate(clickEffect, hit.point += new Vector3(0, 1f, 0), clickEffect.transform.rotation);
+                    StartCoroutine(DestroyClickEffect(_clickEffect.gameObject));
+                }
+        }
+        else
+        {
+            agent.isStopped = true;
+            Debug.Log("ray collided with " + hit.collider.gameObject.name);
+        }
+
+    }
+    private IEnumerator MoveBackAfterCollision()
+    {
+        Vector3 moveBackDirection = -transform.forward * moveBackDistance;
+        Vector3 targetPosition = transform.position + moveBackDirection;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < smoothFactor)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, (elapsedTime / smoothFactor));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+    }
+    private IEnumerator DestroyClickEffect(GameObject _clickEffect) 
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(_clickEffect);
+    }
+
+    private void SetAnimation()
+    {
+        if(agent.velocity == Vector3.zero) 
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+        else 
+        {
+            animator.SetFloat("Speed", 1f);
+        }
+    }
+}
